d06f462f77524a96ed04e13d9d7c588b7621d7d6
Shriti Chandra <shritichan@gmail.com>
2020-08-20 11:06:32 +0530

    initial commit

---
+++ src/Home.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+ 
+class Home extends Component {
+  render() {
+    return (
+      <div>
+      <h3>Welcome!</h3>
+      <h4>This practice site is made with React, React Router, JS, HTML and CSS. </h4> 
+     <a href="https://chandrashritii.github.io/blog/2020/06/28/A-guide-to-Sphinx.html"><button class="button">My blog</button></a>   <a href="https://chandrashritii.github.io/blog/portfolio/"><button class="button">Portfolio</button></a> 
+      </div>
+    );
+  }
+}
+ 
+export default Home;
---
+++ src/Main.js
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+import {
+  Route,
+  NavLink,
+  HashRouter
+} from "react-router-dom";
+import Home from "./Home";
+import Stuff from "./Stuff";
+
+class Main extends Component {
+    render() {
+    return (
+        <HashRouter>
+        <div>
+         <div class="white"><h1>Shriti Chandra</h1></div>
+          <ul className="header">
+            <li><NavLink exact to="/">Home</NavLink></li>
+            <li><NavLink to="/stuff">About</NavLink></li>
+          </ul>
+          <div className="content">
+            <Route exact path="/" component={Home}/>
+            <Route path="/stuff" component={Stuff}/>
+          </div>
+        </div>
+        </HashRouter>
+    );
+    }
+}
+
+export default Main;
+

1cd986b7e7268b9e601a169ab88afeebd9432f35
Shriti <shritichan@gmail.com>
2020-08-20 11:16:00 +0530

    Updates

---
+++ precache-manifest.04babf7ca326758b5e0a16eba2f55ec0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04fa72ed3d455599aa15aab507446ae8",
+    "url": "/panda-overlord/index.html"
+  },
+  {
+    "revision": "158dcca9ff1dd09fb510",
+    "url": "/panda-overlord/static/css/main.c4fbfffc.chunk.css"
+  },
+  {
+    "revision": "b1f898333bf1e99316f3",
+    "url": "/panda-overlord/static/js/2.d595de8b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/panda-overlord/static/js/2.d595de8b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "158dcca9ff1dd09fb510",
+    "url": "/panda-overlord/static/js/main.5fb7590e.chunk.js"
+  },
+  {
+    "revision": "75e18d7d0a443d8e6be4",
+    "url": "/panda-overlord/static/js/runtime-main.36b5468f.js"
+  }
+]);

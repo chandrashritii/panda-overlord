27f5ee38fd9df80f46dbd19c518c314a91d816f1
Shriti Chandra <shritichan@gmail.com>
2022-06-26 07:57:04 +0530

    host
    Signed-off-by: Shriti Chandra <shritichan@gmail.com>

--- src/Main.js
+++ src/Main.js
@@ -1,30 +1,26 @@
 import React, { Component } from "react";
-import {
-  Route,
-  NavLink,
-  HashRouter
-} from "react-router-dom";
-import Home from "./Home";
-import Stuff from "./Stuff";
-import SocialFollow from "./SocialFollow";
+import useScript from 'hooks/useScript';
+
+const MyComponent = props => {
+  useScript('');
+  window.uneeqInteractionsOptions = {
+    personaShareId: "967c7e4f-fea0-4613-9aa3-d37ea3e8abac",
+    layoutMode: 'overlay',
+    showUserInputInterface: true,
+    playWelcome: true,
+    enableMicrophone: true,
+    showUserInputInterface: true,
+    renderContent: true
+}
+  useScript('https://hosted.us.uneeq.io/interactions/v1/deploy');
+}
 
 class Main extends Component {
     render() {
     return (
-        <HashRouter>
-        <div>
-         <div class="white"><h1>Shriti Chandra</h1></div>
-          <ul className="header">
-            <li><NavLink exact to="/">Home</NavLink></li>
-            <li><NavLink to="/stuff">About</NavLink></li>
-          </ul>
-          <div className="content">
-            <Route exact path="/" component={Home}/>
-            <Route path="/stuff" component={Stuff}/>
-          </div>
-          <SocialFollow />
-        </div>
-        </HashRouter>
+      <div className="application">
+    <MyComponent />
+  </div>
     );
     }
 }
--- src/Stuff.js
+++ src/Stuff.js
@@ -1,28 +1,19 @@
 import React, { Component } from "react";
- 
-class Stuff extends Component {
-  render() {
-    return (
-      <div>
-        <h2>About</h2>
-        <h1> Shriti Chandra </h1>
-        <ul>
-        <li> Sophomore Undergrad</li>
-        <li> Google SoD'2020 intern with <a href="https://www.cncf.io/">CNCF</a></li>
-        <li> Intern with <a href="https://layer5.io/">Layer5</a></li>
-        <li> Enthusiastic about opensource, coding, writing, blogging and books.</li>
-        <li> Hit me up for collaborations and projects!</li>
-        </ul>
-       
-       <h3> Major Projects </h3>
-       <ol>
-    <li>Created the <a href="Creating the Service Mesh Performance Specification (SMPS) project site from scratch: ">Service Mesh Performance Specification</a> (SMPS) project site from scratch. To be showcased in the CNCF Kubecon Europe 2020 meet.</li>
-    <li>Researched, wrote and sourced the <a href="https://docs.videolan.me/vlc-user/3.0/en/reference/os_compatibility.html">OS compatibility</a> and the <a href="https://docs.videolan.me/vlc-user/3.0/en/gettingstarted/setup/linux/archlinux.html">archlinux</a> feature documentation using Sphinx and rST for VLC Media Player (VideoLAN community). </li>
-       </ol>
-      Check out my consolidated <a href="https://chandrashritii.github.io/blog/portfolio/">portfolio.</a>  
-      </div>
-    );
-  }
-}
- 
-export default Stuff;
+import { useEffect } from 'react';
+
+      const useScript = url => {
+        useEffect(() => {
+          const script = document.createElement('script');
+      
+          script.src = url;
+          script.async = true;
+      
+          document.body.appendChild(script);
+      
+          return () => {
+            document.body.removeChild(script);
+          }
+        }, [url]);
+      };
+      
+export default useScript;
